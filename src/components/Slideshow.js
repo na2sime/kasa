532dcbf0d6c76c8d1c6850149fb6eb5d3764e88a
na2sime <nassime.abdiou@icloud.com>
2023-05-27 15:35:56 +0200

    Update Slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,5 +1,5 @@
 import '../assets/style/components/slideshow.scss'
-import { useState } from 'react'
+import {useState} from 'react'
 import ArrowRight from '../assets/images/chevron_carousel_right.png'
 import ArrowLeft from '../assets/images/chevron_carousel_left.png'
 
@@ -8,27 +8,23 @@ function Slider({imageSlider}) {
     const [currentIndex, setCurrentIndex] = useState(0)
 
     const nextSlide = () => {
-        setCurrentIndex(currentIndex + 1)
-        if(currentIndex === imageSlider.length - 1)
-            setCurrentIndex(0)
+        setCurrentIndex(currentIndex === imageSlider.length - 1 ? 0 : currentIndex + 1)
     }
 
     const prevSlide = () => {
-        setCurrentIndex(currentIndex - 1)
-        if(currentIndex === 0)
-            setCurrentIndex(imageSlider.length - 1)
+        setCurrentIndex(currentIndex === 0 ? imageSlider.length - 1 : currentIndex - 1);
     }
 
     return (
-        <section style={{backgroundImage : `url(${imageSlider[currentIndex]})`}} className='carousel'>
+        <section style={{backgroundImage: `url(${imageSlider[currentIndex]})`}} className='carousel'>
             {imageSlider.length > 1 &&
                 <>
                     {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
                     <img className='carousel_arrow carousel_arrow_right' src={ArrowRight} alt="Image suivante"
-                        onClick={nextSlide}/>
+                         onClick={nextSlide}/>
                     {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
                     <img className='carousel_arrow carousel_arrow_left' src={ArrowLeft} alt="Image précédente"
-                        onClick={prevSlide}/>
+                         onClick={prevSlide}/>
                     <p className='slideCount'>{currentIndex + 1} / {imageSlider.length}</p>
                 </>
             }
